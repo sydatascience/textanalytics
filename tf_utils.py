ed0b387296e6b16afa166239f56dcf1b9751dae0
maxwbuckley <maxwbuckley@gmail.com>
2017-06-16 22:23:11 +0100

    Factored a lot of reusable stuff out into tf_utils. Added unit tests for the new library also. Updated the linear model to use it. Also added a function for random shuffling my input data.

---
+++ tf_utils.py
@@ -0,0 +1,73 @@
+"""Library of functions for use with tensorflow notebooks."""
+import numpy
+
+
+def normalize_input(input_vector, train_mean, train_std):
+  """Normalize input vector by minusing training mean and dividing by s.d.
+
+  Args:
+    input_vector: numpy.array
+    train_mean: float, mean.
+    train_std: float, standard deviation.
+  Returns:
+    numpy.array with approximately mean 0 and s.d. 1
+  """
+  return (input_vector - train_mean)/train_std
+
+
+def unnormalize_input(normalized_input_vector, train_mean, train_std):
+  """Restor normalized input vector to original scale.
+
+  Args:
+    normalized_input: numpy.array with approximately mean 0 and s.d. 1
+    train_mean: float, mean.
+    train_std: float, standard deviation.
+  Returns:
+    numpy.array on original scale.
+  """
+  return (normalized_input_vector * train_std) + train_mean
+
+
+def unison_shuffled_copies(x, y):
+  """Returns a copy of x and y in which the order of the rows has been shuffled.
+
+  Args:
+    x: numpy.array
+    y: numpy.array
+  Returns:
+    tuple of two numpy.array objects. The first corresponding to x and the
+    second to y
+  Raises:
+    AssertionError: if x and y are not of the same length.
+  """
+  assert len(x) == len(y)
+  p = numpy.random.permutation(len(x))
+  return x[p], y[p]
+
+
+# Function to generate a training batch.
+def generate_batch(batch_size, number_of_batches, max_document_length,
+                   input_data, input_labels):
+  """Creates a generator that generates batches of specified size from the data.
+
+  Args:
+    batch_size: int, what size batch to use.
+    number_of_batches: int, how many batches are generator should provide.
+    max_document_length: int, how long the longest input document was.
+    input_data: X data values.
+    input_labels: y data labels.
+  Returns:
+    Generator that returns tuples of the batch data and the labels.
+  """
+  batch = numpy.ndarray(
+    shape=(batch_size, max_document_length), dtype=numpy.int32)
+  labels = numpy.ndarray(shape=(batch_size, 1), dtype=numpy.float32)
+  batch_index = 0
+  while batch_index < number_of_batches:
+    for element_index in range(batch_size):
+      # Prevents overflow.
+      data_index = ((batch_index * batch_size) + element_index) % len(input_data)
+      batch[element_index] = numpy.array(input_data[data_index])
+      labels[element_index] = input_labels[data_index]
+    batch_index += 1
+    yield batch, labels
---
+++ tf_utils_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import unittest
+import numpy
+import tf_utils
+
+class TestTfUtils(unittest.TestCase):
+  def test_normalize_input(self):
+    input_vector = numpy.array([2, 2, 3, 2, 2])
+    input_mean = input_vector.mean()
+    input_stddev = input_vector.std()
+    normalized_input = tf_utils.normalize_input(input_vector, input_mean, input_stddev)
+    expected_result = numpy.array([-.5, -.5, 2, -.5, -.5])
+    numpy.testing.assert_array_almost_equal(normalized_input, expected_result)
+
+  def test_unnormalize_input(self):
+    input_vector = numpy.array([2, 2, 3, 2, 2])
+    normalized_vector = numpy.array([-.5, -.5, 2, -.5, -.5])
+    input_mean = input_vector.mean()
+    input_stddev = input_vector.std()
+    unnormalized_output = tf_utils.unnormalize_input(
+        normalized_vector, input_mean, input_stddev)
+    numpy.testing.assert_array_almost_equal(unnormalized_output, input_vector)
+
+  def test_normalize_unnormalize_input(self):
+    input_vector = numpy.arange(7)
+    input_mean = input_vector.mean()
+    input_stddev = input_vector.std()
+    normalized_input = tf_utils.normalize_input(input_vector, input_mean, input_stddev)
+    unnormalized_output = tf_utils.unnormalize_input(
+        normalized_input, input_mean, input_stddev)
+    numpy.testing.assert_array_equal(unnormalized_output, input_vector)
+
+  def test_unison_shuffled_copies(self):
+    input_x = numpy.array([[1,2],[3,4],[5,6]])
+    input_y = numpy.array([1,3,5])
+    numpy.random.seed(421)
+    output_x, output_y = tf_utils.unison_shuffled_copies(input_x, input_y)
+    numpy.testing.assert_array_equal(output_x, numpy.array([[1,2],[5,6],[3,4]]))
+    numpy.testing.assert_array_equal(output_y, numpy.array([1,5,3]))
+
+  def test_unison_shuffled_copies(self):
+    input_x = numpy.array([[3, 1, 6],[9, 4, 1],[5, 6, 2],[7, 9, 6]])
+    input_y = numpy.array([1, 3, 5, 11])
+    numpy.random.seed(42)
+    output_x, output_y = tf_utils.unison_shuffled_copies(input_x, input_y)
+    expected_x = numpy.array([[9, 4, 1],[7, 9, 6],[3, 1, 6],[5, 6, 2]])
+    expected_y = numpy.array([3, 11, 1, 5])
+    numpy.testing.assert_array_equal(output_x, expected_x)
+    numpy.testing.assert_array_equal(output_y, expected_y)
+
+  def test_generate_batch(self):
+    input_x = numpy.array([[3, 1, 6],[9, 4, 1],[5, 6, 2],[7, 9, 6]])
+    input_y = numpy.array([1, 3, 5, 11])
+    batch_generator = tf_utils.generate_batch(1, 4, 3, input_x, input_y)
+    for i in range(len(input_x)):
+      x, y = next(batch_generator)
+      numpy.testing.assert_array_equal(x, input_x[i].reshape(1,3))
+      numpy.testing.assert_array_equal(y, input_y[i])
+
+
+if __name__ == '__main__':
+  unittest.main()
